dac049235d313ebb13ead918e50067e9942cd2b5
XenonNek0 <xenonnek0@163.com>
2024-12-28 16:28:18 +0800

    第一次更新

---
+++ Os/os_op.py
@@ -0,0 +1,14 @@
+import os
+
+
+#print(os.path.join('usr', 'bin', 'spam'))
+#
+#myFiles = ['1.txt', '2.txt', '3.txt']
+#for file in myFiles:
+#    print(os.path.join('usr/bin', file))
+
+
+print(os.getcwd())
+os.chdir(/File)
+print(os.getcwd())
+
